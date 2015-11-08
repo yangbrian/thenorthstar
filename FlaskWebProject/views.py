d5f3413d39c7fc4a236ad30ba374860098003c11
Brian <brichen95@gmail.com>
2015-11-08 00:05:18 -0500

    testing ajax...

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -3,8 +3,9 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template, Response
+from flask import render_template, Response, request
 from FlaskWebProject import app
+import json
 import pymysql
 
 
@@ -44,9 +45,41 @@ def about():
 @app.route('/submit', methods=['POST'])
 def submit():
     # return 'Server successfully received data'
+    info = {'start-date': request.form['start-date'],
+            'end-date': request.form['end-date'],
+            'type': request.form['type'],
+            'dest-california': request.form['dest-Cali'],
+            'dest-caribbean': request.form['dest-Caribbean'],
+            'dest-mid-atlantic': request.form['dest-Mid-Atlantic'],
+            'dest-mountain-desert': request.form['dest-MountainDesert'],
+            'dest-northeast': request.form['dest-northeast'],
+            'dest-southwest': request.form['dest-southwest'],
+            'area-los-angeles': request.form['area-LosAngeles'],
+            'area-san-francisco': request.form['area-SanFrancisco'],
+            'area-pr': request.form['area-pr'],
+            'area-dr': request.form['area-dr'],
+            'area-colombia': request.form['area-Colombia'],
+            'area-other-caribbean': request.form['area-OtherCaribbean'],
+            'area-washington-dc': request.form['area-WashingtonDC'],
+            'area-southern': request.form['area-Southern'],
+            'area-west-mountain': request.form['area-WestMountain'],
+            'area-pacific-nw': request.form['area-PacificNW'],
+            'area-alaska': request.form['area-Alaska'],
+            'area-nyc': request.form['area-NYC'],
+            'area-upstate-ny': request.form['area-UpstateNY'],
+            'area-boston': request.form['area-Boston'],
+            'area-midtwest': request.form['area-Midwest'],
+            'area-neislands': request.form['area-NEIslands'],
+            'area-pbi': request.form['area-pbi'],
+            'area-mcd': request.form['area-mcd'],
+            'area-gulf': request.form['area-gulf'],
+            'area-texas': request.form['area-texas'],
+            'area-fill': request.form['area-fill'],
+            'age': request.form['age'],
+            'hometown': request.form['hometown'],
+            'relationship': request.form['relationship']}
 
-    info
-
-    return Response(response=dumps(cursor),
-                    status=200,
-                    mimetype="application/json")
+    return request.form['end-date']
+    # return Response(response='test',
+    #                 status=200,
+    #                 mimetype="application/json")
