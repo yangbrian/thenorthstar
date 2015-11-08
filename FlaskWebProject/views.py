fc24fd473ca77158434e3eb08f9c4a088e4cb68c
Brian Yang <brian@brianyang.me>
2015-11-08 03:09:19 -0500

    Retrieve sets of flight data

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -8,6 +8,7 @@ from contextlib import closing
 from FlaskWebProject import app
 import json
 import pymysql
+from time import strftime
 
 
 @app.route('/')
@@ -48,56 +49,109 @@ def submit():
     # return 'Server successfully received data'
     info = {'start-date': request.form['start-date'],
             'end-date': request.form['end-date'],
-            'type': request.form['type'],
-            'dest-california': request.form['dest-Cali'],
+            'type': request.form.getlist('type'),
+            'dest-california': request.form['dest-California'],
             'dest-caribbean': request.form['dest-Caribbean'],
             'dest-mid-atlantic': request.form['dest-Mid-Atlantic'],
             'dest-mountain-desert': request.form['dest-MountainDesert'],
-            'dest-northeast': request.form['dest-northeast'],
-            'dest-southwest': request.form['dest-southwest'],
-            'area-los-angeles': request.form['area-LosAngeles'],
-            'area-san-francisco': request.form['area-SanFrancisco'],
-            'area-pr': request.form['area-pr'],
-            'area-dr': request.form['area-dr'],
-            'area-colombia': request.form['area-Colombia'],
-            'area-other-caribbean': request.form['area-OtherCaribbean'],
-            'area-washington-dc': request.form['area-WashingtonDC'],
-            'area-southern': request.form['area-Southern'],
-            'area-west-mountain': request.form['area-WestMountain'],
-            'area-pacific-nw': request.form['area-PacificNW'],
-            'area-alaska': request.form['area-Alaska'],
-            'area-nyc': request.form['area-NYC'],
-            'area-upstate-ny': request.form['area-UpstateNY'],
-            'area-boston': request.form['area-Boston'],
-            'area-midwest': request.form['area-Midwest'],
-            'area-neislands': request.form['area-NEIslands'],
-            'area-pbi': request.form['area-pbi'],
-            'area-mcd': request.form['area-mcd'],
-            'area-gulf': request.form['area-gulf'],
-            'area-texas': request.form['area-texas'],
-            'area-fill': request.form['area-fll'],
-            'age': request.form['age'],
-            'hometown': request.form['hometown'],
-            'relationship': request.form['relationship']}
+            'dest-northeast': request.form['dest-Northeast'],
+            'dest-southwest': request.form['dest-Southwest'],
+            # 'area-los-angeles': request.form['area-LosAngeles'],
+            # 'area-san-francisco': request.form['area-SanFrancisco'],
+            # 'area-pr': request.form['area-pr'],
+            # 'area-dr': request.form['area-dr'],
+            # 'area-colombia': request.form['area-Colombia'],
+            # 'area-other-caribbean': request.form['area-OtherCaribbean'],
+            # 'area-washington-dc': request.form['area-WashingtonDC'],
+            # 'area-southern': request.form['area-Southern'],
+            # 'area-west-mountain': request.form['area-WestMountain'],
+            # 'area-pacific-nw': request.form['area-PacificNW'],
+            # 'area-alaska': request.form['area-Alaska'],
+            # 'area-nyc': request.form['area-NYC'],
+            # 'area-upstate-ny': request.form['area-UpstateNY'],
+            # 'area-boston': request.form['area-Boston'],
+            # 'area-midwest': request.form['area-Midwest'],
+            # 'area-neislands': request.form['area-NEIslands'],
+            # 'area-pbi': request.form['area-pbi'],
+            # 'area-mcd': request.form['area-mcd'],
+            # 'area-gulf': request.form['area-gulf'],
+            # 'area-texas': request.form['area-texas'],
+            # 'area-fill': request.form['area-fll'],
+            # 'age': request.form['age'],
+            # 'hometown': request.form['hometown'],
+            # 'relationship': request.form['relationship']
+            }
 
-    hostname = 'us-cdbr-azure-east-a.cloudapp.net'
+    hostname = 'serv.byang.io'
     port = 3306
-    username = 'b67312773cea3d'
-    password = 'cf62f138'
+    username = 'root'
+    password = 'cse305'
     dbName = 'thenorthstar'
+
+    #
+    #  hostname = 'us-cdbr-azure-east-a.cloudapp.net'
+    # port = 3306
+    # username = 'b67312773cea3d'
+    # password = 'cf62f138'
+    # dbName = 'thenorthstar'
+
+
+    conn = pymysql.connect(host = hostname, port = port, user = username, passwd=password, db=dbName)
 
     type_string = ''
     for item in info['type']:
-        type_string += 'city_pair_desttype.destination_type = ' + item + ' OR'
-    type_string = type_string[:len(type_string - 2)]
+        type_string += 'city_pair_desttype.destination_type = ' + item + ' OR '
+    type_string = type_string[:len(type_string)-3]
 
-    conn = pymysql.connect(host = hostname, port = port, user = username, passwd=password, db=dbName)
+
     with closing(conn.cursor()) as cur:
-        sql = "SELECT city_pair_desttype.destination FROM city_pair_desttype WHERE city_pair_desttype.origin = JFK AND (" + type_string + " )"
+        sql = "SELECT DISTINCT city_pair_desttype.destination FROM city_pair_desttype WHERE city_pair_desttype.origin = 'JFK' AND (" + type_string + " )"
         cur.execute(sql)
         conn.commit()
 
-        return(sql)
+        results = []
+
+
+
+        destinations = ''
+        for i in cur.fetchall():
+            itemWeWant = i[0]
+            destinations += "destination = \'" + itemWeWant + "\' OR "
+        destinations = destinations[:len(destinations) - 3]
+
+
+        sql = "SELECT DISTINCT origin, destination, flight_time, flight_date FROM fare WHERE flight_date > \'" + info['start-date']
+        sql += "\' AND flight_date < \'" + info['end-date']
+        sql += "\' AND (" + destinations + ") AND origin=\'JFK\'"
+        cur.execute(sql)
+        conn.commit()
+
+        destinations2 = ''
+        for i2 in cur.fetchall():
+            newArray = []
+            for thingy in i2:
+                newArray.append(str(thingy))
+            results.append(newArray)
+            itemWeWant = i2[1]
+            destinations2 += "code = \'" + itemWeWant + "\' OR "
+        destinations2 = destinations2[:len(destinations2) - 3]
+
+        sql = "SELECT DISTINCT code, latitude, longitude FROM airport_locations WHERE (" + destinations2 + ")"
+        cur.execute(sql)
+        conn.commit()
+        locationMap = []
+        for thing in cur.fetchall():
+            locationMap.append(thing)
+
+        thing2= {'result': results,
+                'locationMap': locationMap
+                }
+
+        return Response(response=json.dumps(thing2),
+                        status=200,
+                        mimetype="application/json")
+
+
     #
     #
     # return Response(response=json.dumps(info),
